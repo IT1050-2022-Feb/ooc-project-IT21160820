864a79b1c4911467530ea8968ba9e80008e63838
IT21160820 <99236002+IT21160820@users.noreply.github.com>
2022-05-09 06:15:09 +0000

    Created classes

---
+++ Assistant.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include "User.h"
+using namespace std;
+
+class Assistant : public User {
+  private:
+    char empID[10];
+  public:
+    Assistant() {
+      strcpy (empID, "");
+    };
+    Assistant(char pName[], char pAddress[], char pEmail[], int pPhone, char pEID[]) : User (pName, pAddress, pEmail, pPhone){
+      strcpy (empID, pEID);
+    };
+    void getEmpID(){
+      cout << "Assistant ID : " << empID << endl;
+    };
+};
---
+++ Nurse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include "User.h"
+using namespace std;
+
+class Nurse : public User {
+  private:
+    char nurseID[10];
+  public:
+    Nurse(){
+      strcpy (nurseID, "");
+    };
+    Nurse(char pName[], char pAddress[], char pEmail[], int pPhone, char pNID[]) : User (pName, pAddress, pEmail, pPhone){
+      strcpy (nurseID, pNID);
+    };
+    void getNurseID() {
+      cout << "Nurse ID : " << nurseID << endl;
+    };
+};
